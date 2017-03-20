d18a3653cb8714139bb26c0968de51314e9ec030
MattiaS <mattia.spinelli@moogsoft.com>
2017-03-20 14:34:44 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const Assert = require('assert'),
+  Hapi = require('hapi'),
+  Path = require('path'),
+    fs = require('fs');
+
+var options = {
+   key  : fs.readFileSync('./server.key'),
+   cert : fs.readFileSync('./server.crt')
+};
+
+const server = new Hapi.Server();
+server.connection({ port: process.env.PORT || 3002, routes: { cors: true }, tls: options });
+
+
+
+server.register(require('inert'), (err) => {
+    if (err) {
+        throw err;
+    }
+
+server.route({
+  method:'GET',
+  path:'/',
+  handler:function(request,reply){
+    reply('im listening')
+  }
+})
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'sounds'
+        }
+    }
+});
+
+server.on('response', function (request) {
+    console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.url.path + ' --> ' + request.response.statusCode);
+});
+
+
+});
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+});
